7b97db8298af6c3ef6269b92eb9d58157b2b481b
Mohamed Ismail <mohamed.g.ismail@orange.com>
2025-07-08 21:03:28 +0300

    add wishlist and cart

--- reverse/settings/common.py
+++ reverse/settings/common.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'shop.context_processors.categories_processor',
+                'constance.context_processors.config',
             ],
         },
     },
