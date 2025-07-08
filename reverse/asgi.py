eb8f01729e45d8c680b10a4eb0a78ab47e54df7f
Mohamed Ismail <mohamed.g.ismail@orange.com>
2025-07-08 18:00:23 +0300

    add badges

--- reverse/asgi.py
+++ reverse/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reverse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reverse.settings.production')
 
 application = get_asgi_application()
